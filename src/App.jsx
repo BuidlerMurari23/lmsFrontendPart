b191fd1c1c03158718e0649970a671e1b6fe31ec
murarigupta <murarigupta28598@gmail.com>
2024-11-02 16:52:19 +0530

    setting up plan vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+   <>
+   <div className='text-gray-500 font-bold text-center mt-4'>Lms frontend...!!!</div>
+   </>
+  )
+}
+
+export default App
